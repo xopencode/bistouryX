aa9f74cc78b950dd917f35d8f6f4f80240ee9e62
xiaodawei <xiaodawei@kaikeba.com>
2021-08-19 19:51:56 +0800

    添加K8S支持

---
+++ bistoury-application/bistoury-application-k8s/src/main/java/qunar/tc/bistoury/application/k8s/service/ApplicationServiceImpl.java
@@ -0,0 +1,39 @@
+package qunar.tc.bistoury.application.k8s.service;
+
+import org.springframework.stereotype.Service;
+import qunar.tc.bistoury.application.api.ApplicationService;
+import qunar.tc.bistoury.application.api.pojo.Application;
+import qunar.tc.bistoury.application.k8s.util.K8SUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author xkrivzooh
+ * @since 2019/8/14
+ */
+@Service
+public class ApplicationServiceImpl implements ApplicationService {
+
+    @Override
+    public List<Application> getAllApplications() {
+        return K8SUtils.getAllAppOrServer(K8SUtils.APPLICATION);
+    }
+
+    @Override
+    public List<Application> getAllApplications(String userCode) {
+        return K8SUtils.getAllAppOrServer(K8SUtils.APPLICATION);
+    }
+
+    @Override
+    public List<String> getAppOwner(String appCode) {
+        return new ArrayList<>();
+    }
+
+    @Override
+
+    public int save(Application application, String loginUser, boolean admin) {
+        return 0;
+    }
+
+}
